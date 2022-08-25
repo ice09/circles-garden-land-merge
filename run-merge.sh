4c82c32a4684544495fd6181b3dfba769a504a4a
Alexander C <alexander.culum@gmail.com>
2022-08-25 18:50:22 +0200

    Remove defaults for values.

--- run-merge.sh
+++ run-merge.sh
@@ -1,3 +1,3 @@
-export CIRCLES_GARDENS_CSV_URL=https://circles-usernames.s3.eu-central-1.amazonaws.com/user-data-20220805.csv
-export CIRCLES_LAND_GQL_URL=https://dev.api.circles.land
+export CIRCLES_GARDENS_CSV_URL=
+export CIRCLES_LAND_GQL_URL=
 java -jar target/circles-identity-service-0.0.1-SNAPSHOT.jar
